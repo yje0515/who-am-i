2ad25b18e2375444026786e871ffb79067f0fdf6
yje0515 <155126222+yje0515@users.noreply.github.com>
2026-01-28 19:49:51 +0900

    init

---
+++ src/main/java/com/yje/whoami/WhoamiApiApplication.java
@@ -0,0 +1,13 @@
+package com.yje.whoami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoamiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoamiApiApplication.class, args);
+	}
+
+}
